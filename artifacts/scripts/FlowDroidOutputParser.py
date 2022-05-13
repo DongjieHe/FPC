47d41943836e0d75f32ee575d6c959b047d21cab
DongjieHe <dongjieh@cse.unsw.edu.au>
2022-05-14 00:09:24 +1000

    update

--- artifacts/scripts/FlowDroidOutputParser.py
+++ artifacts/scripts/FlowDroidOutputParser.py
@@ -27,6 +27,8 @@ def extractXmlMap(xml):
         ret["SourceCount"] = 0
     if "SinkCount" not in ret:
         ret["SinkCount"] = 0
+    if "PropagationCount" not in ret:
+        ret["PropagationCount"] = 0
     return ret
 
 def outputList(outdir):
@@ -50,20 +52,20 @@ def outputMap(outdir):
 
 def buildTable(outList):
     table = PrettyTable()
-    table.field_names = ["APP", "CG construction time (s)", "IFDS Time (s)", "Max Memory (MB)", "#Leaks", "#Sources", "#Sinks"]
+    table.field_names = ["APP", "CG construction time (s)", "IFDS Time (s)", "Max Memory (MB)", "#PathEdges", "#Leaks", "#Sources", "#Sinks"]
     for xmlMap in outList:
-        table.add_row([xmlMap["APP"], xmlMap["CallgraphConstructionSeconds"], xmlMap["TaintPropagationSeconds"], xmlMap["MaxMemoryConsumption"], xmlMap["LeakNum"], xmlMap["SourceCount"], xmlMap["SinkCount"]])
+        table.add_row([xmlMap["APP"], xmlMap["CallgraphConstructionSeconds"], xmlMap["TaintPropagationSeconds"], xmlMap["MaxMemoryConsumption"], xmlMap["PropagationCount"], xmlMap["LeakNum"], xmlMap["SourceCount"], xmlMap["SinkCount"]])
     return table
 
 def buildTable2(outMap, outMap2):
     table = PrettyTable()
-    table.field_names = ["APP", "CG construction time (s)", "IFDS Time (s)", "Max Memory (MB)", "#Leaks", "#Sources", "#Sinks"]
+    table.field_names = ["APP", "CG construction time (s)", "IFDS Time (s)", "Max Memory (MB)", "#PathEdges", "#Leaks", "#Sources", "#Sinks"]
     for app in outMap:
         if app in outMap2:
             xmlMap = outMap[app]
             xmlMap2 = outMap2[app]
-            table.add_row([app, xmlMap["CallgraphConstructionSeconds"], xmlMap["TaintPropagationSeconds"], xmlMap["MaxMemoryConsumption"], xmlMap["LeakNum"], xmlMap["SourceCount"], xmlMap["SinkCount"]])
-            table.add_row(["", xmlMap2["CallgraphConstructionSeconds"], xmlMap2["TaintPropagationSeconds"], xmlMap2["MaxMemoryConsumption"], xmlMap2["LeakNum"], xmlMap2["SourceCount"], xmlMap2["SinkCount"]])
+            table.add_row([app, xmlMap["CallgraphConstructionSeconds"], xmlMap["TaintPropagationSeconds"], xmlMap["MaxMemoryConsumption"], xmlMap["PropagationCount"], xmlMap["LeakNum"], xmlMap["SourceCount"], xmlMap["SinkCount"]])
+            table.add_row(["", xmlMap2["CallgraphConstructionSeconds"], xmlMap2["TaintPropagationSeconds"], xmlMap2["MaxMemoryConsumption"], xmlMap2["PropagationCount"], xmlMap2["LeakNum"], xmlMap2["SourceCount"], xmlMap2["SinkCount"]])
     return table
 
 # Main
@@ -72,7 +74,7 @@ if __name__ == '__main__':
     # print(extractXmlMap(outputxml))
     # outdir = "/home/hedj/Work/OnlineIFDS/artifacts/output/run1"
     # outdir2 = "/home/hedj/Work/OnlineIFDS/artifacts/output/run2"
-    outdir = "/home/hedj/Work/OnlineIFDS/artifacts/output/qilin/FDroid/connectivity/"
-    outdir2 = "/home/hedj/Work/OnlineIFDS/artifacts/output/tower/FDroid/connectivity/"
+    outdir = "/home/hedj/Work/OnlineIFDS/artifacts/output/qilin/FDroid/graphics/"
+    outdir2 = "/home/hedj/Work/OnlineIFDS/artifacts/output/tower/FDroid/graphics/"
     table = buildTable2(outputMap(outdir), outputMap(outdir2))
     print(table)
